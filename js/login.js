2121799821bfedf7a114388b8a4d6ed511c83bf0
Dylan Montoya <63882463+DylanMontoya@users.noreply.github.com>
2022-08-21 20:44:00 -0500

    publish website
    load files

---
+++ js/login.js
@@ -0,0 +1,14 @@
+(function login(){
+    const openLogin = document.querySelector('.cta');
+    const login = document.querySelector('.cta');
+    
+
+
+    openButton.addEventListener('click', ()=>{
+         menu.classList.add('nav__link--show');
+    });
+    closeMenu.addEventListener('click', ()=>{
+        menu.classList.remove('nav__link--show')
+    });
+
+})();
